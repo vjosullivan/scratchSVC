7338f780e3b3795a243a30d8fbd4ef464c7dfd17
Vince O'Sullivan (from laptop) <vjosullivan@gmail.com>
2017-11-16 07:42:08 +0000

    Model (placeholder) added.

---
+++ scratchSVC/model/DataLoader.swift
@@ -0,0 +1,13 @@
+//
+//  ApplicationLoader.swift
+//  scratchSVC
+//
+//  Created by Vincent O'Sullivan on 15/11/2017.
+//  Copyright © 2017 Vincent O'Sullivan. All rights reserved.
+//
+
+import Foundation
+
+protocol DataLoader {
+    
+}
---
+++ scratchSVC/model/SampleDataLoader.swift
@@ -0,0 +1,13 @@
+//
+//  SampleDataLoader.swift
+//  scratchSVC
+//
+//  Created by Vincent O'Sullivan on 15/11/2017.
+//  Copyright © 2017 Vincent O'Sullivan. All rights reserved.
+//
+
+import Foundation
+
+class SampleDataLoader: DataLoader {
+
+}
